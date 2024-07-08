3e2fae732158ce5b3c15523a5af6dca582b2d6f7
rgs2151 <rgs2151@columbia.edu>
2024-07-08 04:57:16 -0400

    hack redir logic

--- Apollo/static/js/global_manager.js
+++ Apollo/static/js/global_manager.js
@@ -3,9 +3,20 @@
 $(document).ready(function() {
     // First check if the user is logged in. If not, redirect to the login page
 
-    // Check if localStorage.getItem("token") exists
-    // If it does not exist, redirect to the login page
-    if (localStorage.getItem("token") === null) {
-        window.location.href = "/user/signin/";
+    // Hack for now:
+    //  check if your url is not base/user/signin/ or base/user/signup/
+    //  if anything has user in it, then dont redirect it
+
+    // Get the current URL
+    var url = window.location.href;
+
+    // Check if the URL contains "user" in it
+    if (url.includes("user") === false) {
+        // Check if localStorage.getItem("token") exists
+        // If it does not exist, redirect to the login page
+        if (localStorage.getItem("token") === null) {
+            window.location.href = "/user/signin/";
+        }
     }
+
 });
